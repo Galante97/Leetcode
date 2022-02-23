0697acaf0437c80513fff169aa6d3b9a0f2b73fd
James Galante <Jgalante@udel.edu>
2022-02-22 21:25:19 -0500

    2/22

---
+++ Code Signal/Interview Practice/Arrays/442-FindAllDuplicatesInAnArray.java
@@ -0,0 +1,31 @@
+/*
+ * 442. Find All Duplicates in an Array
+ * Given an integer array nums of length n where all the integers of nums are in
+ * the range [1, n] and each integer appears once or twice, return an array of
+ * all the integers that appears twice.
+ * 
+ * You must write an algorithm that runs in O(n) time and uses only constant
+ * extra space.
+ *
+ * Example 1:
+    * Input: nums = [4,3,2,7,8,2,3,1]
+    * Output: [2,3]
+ */
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        HashSet<Integer> unq = new HashSet<>();
+
+        List<Integer> out = new ArrayList<>();
+
+        for (int num : nums) {
+            if (unq.contains(num)) {
+                out.add(num);
+            } else {
+                unq.add(num);
+            }
+        }
+
+        return out;
+
+    }
+}
